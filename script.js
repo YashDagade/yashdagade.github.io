a5268b5422cad7209c744e27e2159c3efe37e2b6
Yash Dagade <yash21.dagade@gmail.com>
2024-06-12 17:44:43 -0500

    Update project links and add hover images for projects

--- script.js
+++ script.js
@@ -6,12 +6,32 @@ document.addEventListener('DOMContentLoaded', function() {
         link.addEventListener('mouseenter', function() {
             bodyContent.classList.add('fade');
             this.classList.add('highlight');
-            this.querySelector('.project-text').classList.add('highlight');
+
+            const imageSrc = this.getAttribute('data-image');
+            if (imageSrc) {
+                let hoverImage = document.createElement('img');
+                hoverImage.src = imageSrc;
+                hoverImage.classList.add('hover-image');
+                document.body.appendChild(hoverImage);
+
+                this.hoverImage = hoverImage;
+            }
         });
+
         link.addEventListener('mouseleave', function() {
             bodyContent.classList.remove('fade');
             this.classList.remove('highlight');
-            this.querySelector('.project-text').classList.remove('highlight');
+            if (this.hoverImage) {
+                this.hoverImage.remove();
+                this.hoverImage = null;
+            }
+        });
+
+        link.addEventListener('mousemove', function(e) {
+            if (this.hoverImage) {
+                this.hoverImage.style.left = `${e.clientX - this.hoverImage.offsetWidth / 2}px`;
+                this.hoverImage.style.top = `${e.clientY - this.hoverImage.offsetHeight / 2 }px`;
+            }
         });
     });
 });
